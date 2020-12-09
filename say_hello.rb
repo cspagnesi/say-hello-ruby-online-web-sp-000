168c58ed18dd5ab4ab44734a2cc4876d578edd17
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-12-09 17:32:22 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,20 @@
-# Build your say_hello method here
+def workout_schedule(day, muscle_group)
+  if day == "monday"
+    if muscle_group == "chest"
+      puts "benchpress"
+    elsif muscle_group == "tricep"
+      puts "tricep pushdown"
+    elsif muscle_group == "back"
+      puts "lat pulldown"
+  end
+  elsif day == "tuesday"
+      if muscle_group == "chest"
+        puts "chest fly"
+      if muscle_group == "tricep"
+        puts "dips"
+      if muscle_group == "back"
+        puts "pull-ups"
+      end
+    end
+  end
+  
